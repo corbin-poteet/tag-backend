47151945806f3c5d72a4d2ad23dadc4e47ad7673
Corbin Poteet <corbin.poteet@gmail.com>
2024-04-01 16:19:04 -0500

    Initial commit

---
+++ src/main/java/com/thrive/tagbackend/TagBackendApplication.java
@@ -0,0 +1,13 @@
+package com.thrive.tagbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagBackendApplication.class, args);
+	}
+
+}
