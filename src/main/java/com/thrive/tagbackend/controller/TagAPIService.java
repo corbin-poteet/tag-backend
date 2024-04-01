a8834a8b34fad3d2affa81cca5cbb1b4e46e93f0
Corbin Poteet <corbin.poteet@gmail.com>
2024-04-01 18:19:17 -0500

    Create basic CRUD API calls

--- src/main/java/com/thrive/tagbackend/controller/TagAPIService.java
+++ src/main/java/com/thrive/tagbackend/controller/TagAPIService.java
@@ -1,26 +1,33 @@
 package com.thrive.tagbackend.controller;
 
 import com.thrive.tagbackend.model.Tag;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tag")
 public class TagAPIService {
+    private Tag tag;
 
     @GetMapping("/{group}/{element}")
-    public Tag getTag(@PathVariable String group, @PathVariable String element) {
-        return new Tag("0010,0010", "Patient's Name", "PN", "64", "1", "John Doe", 16, 16, true);
+    public Tag getTag(@PathVariable int group, @PathVariable int element) {
+        return tag;
     }
 
-    @GetMapping("/all")
-    public Tag[] getAllTags() {
-        return new Tag[] {
-                new Tag("0010,0010", "Patient's Name", "PN", "64", "1", "John Doe", 16, 16, true),
-                new Tag("0010,0020", "Patient ID", "LO", "64", "1", "12345", 16, 32, true),
-                new Tag("0010,0030", "Patient's Birth Date", "DA", "8", "1", "19900101", 16, 48, true),
-        };
+    @PostMapping()
+    public String createTag(@RequestBody Tag tag) {
+        this.tag = tag;
+        return "Tag created successfully!";
+    }
+
+    @PutMapping()
+    public String updateTag(@RequestBody Tag tag) {
+        this.tag = tag;
+        return "Tag updated successfully!";
+    }
+
+    @DeleteMapping("/{group}/{element}")
+    public String deleteTag(@PathVariable int group, @PathVariable int element) {
+        this.tag = null;
+        return "Tag deleted successfully!";
     }
 }
