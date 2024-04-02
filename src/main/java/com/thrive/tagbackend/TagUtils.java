037dc58276382cd99b872cadb1e28339ce5aef33
Corbin Poteet <corbin.poteet@gmail.com>
2024-04-02 00:08:30 -0500

    Remove debug print lines

--- src/main/java/com/thrive/tagbackend/TagUtils.java
+++ src/main/java/com/thrive/tagbackend/TagUtils.java
@@ -2,18 +2,13 @@ package com.thrive.tagbackend;
 
 public class TagUtils {
     public static String getGroupAndElement(int group, int element) {
-        System.out.println("groupAndElement: " + group + "," + element);
-
-
         String groupHex = Integer.toHexString(group);
         String paddedGroupHex = "0".repeat(4 - groupHex.length()) + groupHex;
 
         String elementHex = Integer.toHexString(element);
         String paddedElementHex = "0".repeat(4 - elementHex.length()) + elementHex;
 
-        String groupAndElement = paddedGroupHex + "," + paddedElementHex;
-        System.out.println("groupAndElement: " + groupAndElement);
-        return groupAndElement;
+        return paddedGroupHex + "," + paddedElementHex;
     }
 
     public static int getGroup(String groupAndElement) {
