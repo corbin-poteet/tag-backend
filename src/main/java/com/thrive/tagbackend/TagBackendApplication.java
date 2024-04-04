0c893618d0dba299361c33a2fbdf2e785701b091
Corbin Poteet <corbin.poteet@gmail.com>
2024-04-03 20:35:27 -0500

    Add simple gui

--- src/main/java/com/thrive/tagbackend/TagBackendApplication.java
+++ src/main/java/com/thrive/tagbackend/TagBackendApplication.java
@@ -2,16 +2,24 @@ package com.thrive.tagbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import javax.swing.*;
 
 @SpringBootApplication()
 public class TagBackendApplication {
 
+	private static final int WIDTH = 300;
+	private static final int HEIGHT = 100;
 	public static void main(String[] args) {
+
+		JFrame frame = new JFrame("Tag CRUD Application Server");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(WIDTH,HEIGHT);
+
+		JLabel label = new JLabel("Running server...", SwingConstants.CENTER);
+		frame.getContentPane().add(label);
+
+		frame.setVisible(true);
+
 		SpringApplication.run(TagBackendApplication.class, args);
 	}
-
 }
